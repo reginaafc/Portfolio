fd20a4099fd193c0f1edc5590d6101580f3076e7
Regina Franco <79554515+reginaafc@users.noreply.github.com>
2025-04-17 18:49:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('contactarBtn');
+    btn.addEventListener('click', () => {
+      alert('Gracias por interesarte. ¡Te contactaré pronto!');
+    });
+  });
+  
